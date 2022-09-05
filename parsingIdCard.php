1f9e406c474f2fa9f646201bb7d19bb37f0044aa
gevanlolyvich <85344506+gevanlolyvich@users.noreply.github.com>
2022-09-05 12:04:41 +0700

    Update parsingIdCard.php

--- parsingIdCard.php
+++ parsingIdCard.php
@@ -1,5 +1,5 @@
 <?php
-require 'function.php';
+require 'koneksi.php';
 ?>*<?php
 $ambilnama= mysqli_query($conn,"select * from users");
 while($data=mysqli_fetch_array($ambilnama)){$nama = $data['id_user'];?><?=$nama;?>,<?php
